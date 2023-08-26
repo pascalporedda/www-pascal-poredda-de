299189dfe05d274f0628648ccabea94fb1adf208
Pascal Poredda <pascal@poredda.digital>
2023-08-26 09:46:03 +0200

    change up some bio stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,83 +1,83 @@
-import { about, name, short_bio } from "@/lib/about";
-import Image from "next/image";
-import avatar from "@/app/avatar.jpg";
+import { about, name, short_bio } from '@/lib/about';
+import Image from 'next/image';
+import avatar from '@/app/avatar.jpg';
 import {
   ArrowRightIcon,
   CountdownTimerIcon,
   GitHubLogoIcon,
   InstagramLogoIcon,
-} from "@radix-ui/react-icons";
-import Link from "next/link";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+} from '@radix-ui/react-icons';
+import Link from 'next/link';
+import { Card, CardContent, CardFooter } from '@/components/ui/card';
 
 export default async function Home() {
   return (
-    <Card className="max-w-2xl">
+    <Card className='max-w-2xl'>
       <CardContent>
-        <h1 className="font-bold text-3xl mt-2">{name} Poredda</h1>
-        <p className="my-5 max-w-[460px] text-neutral-800 dark:text-neutral-200">
+        <h1 className='font-bold text-3xl mt-2'>{name} Poredda</h1>
+        <p className='my-5 max-w-[460px] text-neutral-800 dark:text-neutral-200'>
           {short_bio()}
         </p>
-        <div className="flex items-start sm:items-center my-8 flex-col sm:flex-row">
+        <div className='flex items-start sm:items-center my-8 flex-col sm:flex-row'>
           <Image
             alt={name}
-            className="rounded-full grayscale"
+            className='rounded-full grayscale'
             src={avatar}
-            placeholder="blur"
+            placeholder='blur'
             width={100}
             priority
           />
-          <div className="mt-8 sm:mt-0 ml-0 sm:ml-6 space-y-2 text-neutral-500 dark:text-neutral-400">
+          <div className='mt-8 sm:mt-0 ml-0 sm:ml-6 space-y-2 text-neutral-500 dark:text-neutral-400'>
             <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://instagram.com/pascal.poredda"
-              className="flex items-center gap-2"
+              rel='noopener noreferrer'
+              target='_blank'
+              href='https://instagram.com/pascal.poredda'
+              className='flex items-center gap-2'
             >
               <InstagramLogoIcon />
-              {124} followers on Instagram
+              @pascal.poredda
             </a>
             <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://github.com/pascalporedda"
-              className="flex items-center gap-2"
+              rel='noopener noreferrer'
+              target='_blank'
+              href='https://github.com/pascalporedda'
+              className='flex items-center gap-2'
             >
               <GitHubLogoIcon />
-              {`${7823} stars on GitHub`}
+              @pascalporedda
             </a>
-            <Link href="/blog" className="flex items-center gap-2">
+            <Link href='/blog' className='flex items-center gap-2'>
               <CountdownTimerIcon />
-              {`${6227} blog views `}
+              read the blog
             </Link>
           </div>
         </div>
-        <p className="my-5 max-w-[600px] text-neutral-800 dark:text-neutral-200">
+        <p className='my-5 max-w-[600px] text-neutral-800 dark:text-neutral-200'>
           {about()}
         </p>
       </CardContent>
       <CardFooter>
-        <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-500 dark:text-neutral-400">
+        <ul className='flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-500 dark:text-neutral-400'>
           <li>
             <a
-              className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all space-x-2"
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://instagram.com/pascal.poredda"
+              className='flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all space-x-2'
+              rel='noopener noreferrer'
+              target='_blank'
+              href='https://instagram.com/pascal.poredda'
             >
               <ArrowRightIcon />
-              <p className="h-7">follow me on Instagram</p>
+              <p className='h-7'>follow me on Instagram</p>
             </a>
           </li>
           <li>
             <a
-              className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all space-x-2"
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://youtube.com/@pascalporedda"
+              className='flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all space-x-2'
+              rel='noopener noreferrer'
+              target='_blank'
+              href='https://youtube.com/@pascalporedda'
             >
               <ArrowRightIcon />
-              <p className="h-7">subscribe to my YouTube</p>
+              <p className='h-7'>subscribe to my YouTube</p>
             </a>
           </li>
         </ul>
--- lib/about.tsx
+++ lib/about.tsx
@@ -1,20 +1,19 @@
 export const short_bio = () => {
   return (
     <>
-      Hey, I&apos;m {name}. I&apos;m the CTO of EuropeCarTrader, freelancer by
-      day and a Zouk passionate at night.
+      Hey, I&apos;m {name}. A freelancer by day and rock-climbing-dancer
+      whenever else.
     </>
   );
 };
 
-export const name = "Pascal";
+export const name = 'Pascal';
 
 export const about = () => {
   return (
     <>
-      I love coding as much as I love dancing, and rock-climbing. Striving and
-      struggling to progress everything at once. And I recently started to learn
-      DJing.
+      I love coding as much as I love dancing, as much as I love rock-climbing.
+      Striving and struggling to progress everything at once.
     </>
   );
 };
