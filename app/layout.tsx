861a2505ccd5a37ce2123927afb9321586e49e53
Pascal Poredda <pascal@poredda.digital>
2024-05-20 15:34:59 +0200

    fixup some issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,14 +60,12 @@ export default function RootLayout({
       )}
     >
       <body className='antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto'>
-        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-          <main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
-            <Navbar />
-            {children}
-            <Analytics />
-            <SpeedInsights />
-          </main>
-        </ThemeProvider>
+        <main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
+          <Navbar />
+          {children}
+          <Analytics />
+          <SpeedInsights />
+        </main>
       </body>
     </html>
   );
