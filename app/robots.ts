a39767fc4f1e6715036ed08621f2a24b0f3ce677
Pascal Poredda <pascal@poredda.digital>
2024-06-25 11:38:35 +0200

    headers + redirects

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,7 @@ export default function robots() {
     rules: [
       {
         userAgent: '*',
+        disallow: ['_next/', 'api/'],
       },
     ],
     sitemap: 'https://pascal-poredda.de/sitemap.xml',
